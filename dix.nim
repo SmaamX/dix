d4134a793dada7a75862ae66e871d404648fbbd2
SmaamX <90418723+SmaamX@users.noreply.github.com>
2025-03-19 13:06:23 +0330

    Add files via upload

--- dix.nim
+++ dix.nim
@@ -1,79 +1,97 @@
-import sequtils, strutils, math
+import sequtils, strutils, math, tables
 
-proc getUniqueCharsAndCount(inputFile: string): (string, seq[(char, int)]) =
+proc getUniqueChunks(inputFile: string): (seq[string], seq[int]) =
   let data = readFile(inputFile)
-  var charCount: seq[(char, int)] = @[]
-  var uniqueChars = ""
-  for c in data:
-    var found = false
-    for i in 0 ..< charCount.len:
-      if charCount[i][0] == c:
-        charCount[i] = (c, charCount[i][1] + 1)
-        found = true
-        break
-    if not found:
-      charCount.add((c, 1))
-      uniqueChars &= c
-  return (uniqueChars, charCount)
+  var chunkMap = initTable[string, int]()
+  var uniqueChunks: seq[string] = @[]
+  var indices: seq[int] = @[]
+  let chunkSize = 4
+  for i in countup(0, data.len - 1, chunkSize):
+    let chunk = if i + chunkSize - 1 < data.len: data[i .. i + chunkSize - 1] else: data[i .. ^1] & "\0".repeat(chunkSize - (data.len - i))
+    if not chunkMap.hasKey(chunk):
+      chunkMap[chunk] = uniqueChunks.len
+      uniqueChunks.add(chunk)
+    indices.add(chunkMap[chunk])
+  return (uniqueChunks, indices)
 
 proc compress(inputFile, compressedFile: string) =
   let data = readFile(inputFile)
-  let (uniqueChars, charCount) = getUniqueCharsAndCount(inputFile)
-  let bitsPerChar = ceil(log2(float(uniqueChars.len))).int
+  let (uniqueChunks, indices) = getUniqueChunks(inputFile)
+  let bitsPerIndex = max(1, ceil(log2(float(uniqueChunks.len))).int)
   var compressed: seq[byte] = @[]
-  compressed.add(byte(uniqueChars.len shr 8))
-  compressed.add(byte(uniqueChars.len))
-  compressed.add(byte(bitsPerChar))
+  compressed.add(byte(uniqueChunks.len shr 8))
+  compressed.add(byte(uniqueChunks.len))
+  compressed.add(byte(bitsPerIndex))
   compressed.add(byte(data.len shr 24))
   compressed.add(byte(data.len shr 16))
   compressed.add(byte(data.len shr 8))
   compressed.add(byte(data.len))
-  for c in uniqueChars:
-    compressed.add(byte(c))
-  var bits: uint64 = 0
+  for chunk in uniqueChunks:
+    for c in chunk:
+      compressed.add(byte(c))
+  var bits: seq[byte] = @[]
   var bitCount = 0
-  for c in data:
-    let index = uniqueChars.find(c)
-    bits = (bits shl bitsPerChar) or uint64(index)
-    bitCount += bitsPerChar
-    if bitCount >= 8:
-      compressed.add(byte(bits shr (bitCount - 8)))
-      bits = bits and ((1'u64 shl (bitCount - 8)) - 1)
-      bitCount -= 8
+  var currentByte = 0'u8
+  for idx in indices:
+    var value = uint64(idx)
+    var bitsLeft = bitsPerIndex
+    while bitsLeft > 0:
+      let bitsToWrite = min(8 - bitCount, bitsLeft)
+      let shift = bitsLeft - bitsToWrite
+      currentByte = currentByte or uint8((value shr shift) and ((1'u64 shl bitsToWrite) - 1)) shl (8 - bitCount - bitsToWrite)
+      bitCount += bitsToWrite
+      bitsLeft -= bitsToWrite
+      if bitCount >= 8:
+        bits.add(currentByte)
+        currentByte = 0'u8
+        bitCount = 0
+      value = value and ((1'u64 shl shift) - 1)
   if bitCount > 0:
-    compressed.add(byte(bits shl (8 - bitCount)))
+    bits.add(currentByte)
+  compressed.add(bits)
   writeFile(compressedFile, compressed)
 
 proc decompress(compressedFile, outputFile: string) =
   let compressed = readFile(compressedFile).toSeq.mapIt(byte(it))
-  let uniqueCharsLen = (int(compressed[0]) shl 8) or int(compressed[1])
-  let bitsPerChar = int(compressed[2])
+  let uniqueChunksLen = (int(compressed[0]) shl 8) or int(compressed[1])
+  let bitsPerIndex = int(compressed[2])
   let originalSize = (int(compressed[3]) shl 24) or (int(compressed[4]) shl 16) or (int(compressed[5]) shl 8) or int(compressed[6])
-  var uniqueChars = ""
-  for i in 7 ..< 7 + uniqueCharsLen:
-    uniqueChars &= char(compressed[i])
-  let headerSize = 7 + uniqueCharsLen
+  var uniqueChunks: seq[string] = @[]
+  let chunkSize = 4
+  var chunkStart = 7
+  for i in 0 ..< uniqueChunksLen:
+    uniqueChunks.add(compressed[chunkStart .. chunkStart + chunkSize - 1].mapIt(char(it)).join(""))
+    chunkStart += chunkSize
+  let headerSize = 7 + uniqueChunksLen * chunkSize
   if headerSize >= compressed.len:
     return
   let compressedData = compressed[headerSize .. ^1]
-  var decompressed: seq[byte] = @[]
+  var indices: seq[int] = @[]
   var bits: uint64 = 0
   var bitCount = 0
   for b in compressedData:
     bits = (bits shl 8) or uint64(b)
     bitCount += 8
-    while bitCount >= bitsPerChar and decompressed.len < originalSize:
-      let mask = (1'u64 shl bitsPerChar) - 1
-      let index = int((bits shr (bitCount - bitsPerChar)) and mask)
-      decompressed.add(byte(uniqueChars[index]))
-      bitCount -= bitsPerChar
+    while bitCount >= bitsPerIndex:
+      let mask = (1'u64 shl bitsPerIndex) - 1
+      let index = int((bits shr (bitCount - bitsPerIndex)) and mask)
+      if index < uniqueChunks.len:
+        indices.add(index)
+      bitCount -= bitsPerIndex
       bits = bits and ((1'u64 shl bitCount) - 1)
+  var decompressed: seq[byte] = @[]
+  for idx in indices:
+    if decompressed.len < originalSize:
+      let chunk = uniqueChunks[idx]
+      for c in chunk:
+        if decompressed.len < originalSize:
+          decompressed.add(byte(c))
   writeFile(outputFile, decompressed)
 
 when isMainModule:
   let inputFile = "input.txt"
   let compressedFile = "compressed.dix"
-  let outputFile = "dinput.txt"
+  let outputFile = "d_input.txt"
   echo "Compressing..."
   compress(inputFile, compressedFile)
   echo "Compressed to ", compressedFile, " (size: ", readFile(compressedFile).len, " bytes)"
